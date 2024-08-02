cf59a07779c842a138f8377dd93b4d572fd2e53d
Makiya <107805761+makiya32@users.noreply.github.com>
2024-08-02 13:52:39 -0400

    Create blockchain_mock.py

---
+++ blockchain_mock.py
@@ -0,0 +1,42 @@
+
+import hashlib
+class SandDollarBlock:
+    
+    def __init__(self, previous_block_hash, transaction_list):
+        self.previous_block_hash = previous_block_hash
+        self.transaction_list = transaction=transaction_list
+
+        self.block_data = "--->".join(transaction_list) + "--->" + previous_block_hash
+        self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
+
+def main():
+#   Transactions:
+    t1 = "Niesha sends 7 SD to Vyvyan"
+    t2 = "Makiya sends 2 SD to Vyvyan"
+    t3 = "Makiya sends 5 SD to Fabiola"
+    t4 = "Fabiola sends 4.3 SD to Niesha"
+    t5 = "George sends 6 SD to Makiya"
+    t6 = "Makiya sends 9 SD to Niesha"
+
+    #1 Block:
+    initial_block = SandDollarBlock("Initial String", [t1, t2])
+
+    print("Initial Block Data: ",initial_block.block_data, "\n")
+    print("Initial Hash: " , initial_block.block_hash, "\n")
+    
+    #2 Block:
+    second_block = SandDollarBlock("Initial String", [t3, t4])
+
+    print("Second Block Data: ",second_block.block_data, "\n")
+    print("Second Hash: " , second_block.block_hash, "\n")
+    
+    #3 Block:
+    third_block = SandDollarBlock("Initial String", [t5, t6])
+
+    print("Third Block Data: ", third_block.block_data, "\n")
+    print("Third Hash: " , third_block.block_hash, "\n")
+
+#Allow script to be run directly:
+if __name__ == "__main__":
+     main()
+            
